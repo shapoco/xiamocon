9b80777f68a81390d669e2957392edabc4eec333
shapoco <40152459+shapoco@users.noreply.github.com>
2026-03-21 14:39:49 +0000

    Deploying to gh-pages from @ shapoco/xiamocon@0a9e7e0adaf04279e03c5de14fb09ef420d825b9 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -6,5 +6,5 @@ var searchData=
   ['conjugate_3',['conjugate',['../structxmc_1_1quat.html#aced56cd1f42a10cef5b48806c4919bea',1,'xmc::quat']]],
   ['contains_4',['contains',['../structxmc_1_1rect__t.html#ad58b7f607248c723b720f2899d3f5bfc',1,'xmc::rect_t']]],
   ['context_5',['context',['../structxmc__repeating__timer__t.html#a19e58d9855a8ecd93c1e71880c875b23',1,'xmc_repeating_timer_t']]],
-  ['cross_6',['cross',['../structxmc_1_1vec3.html#ad8f508d0cce888aa961f6df7a6dbdc8e',1,'xmc::vec3']]]
+  ['cross_6',['cross',['../structxmc_1_1vec2.html#a5e1261a51467c69e3001250876032c5e',1,'xmc::vec2::cross()'],['../structxmc_1_1vec3.html#ad8f508d0cce888aa961f6df7a6dbdc8e',1,'xmc::vec3::cross()']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -3,5 +3,5 @@ var searchData=
   ['clip_5frect_0',['clip_rect',['../geo__common_8hpp.html#aef6134e7c220585cb151949b39b36198',1,'xmc']]],
   ['conjugate_1',['conjugate',['../structxmc_1_1quat.html#aced56cd1f42a10cef5b48806c4919bea',1,'xmc::quat']]],
   ['contains_2',['contains',['../structxmc_1_1rect__t.html#ad58b7f607248c723b720f2899d3f5bfc',1,'xmc::rect_t']]],
-  ['cross_3',['cross',['../structxmc_1_1vec3.html#ad8f508d0cce888aa961f6df7a6dbdc8e',1,'xmc::vec3']]]
+  ['cross_3',['cross',['../structxmc_1_1vec2.html#a5e1261a51467c69e3001250876032c5e',1,'xmc::vec2::cross()'],['../structxmc_1_1vec3.html#ad8f508d0cce888aa961f6df7a6dbdc8e',1,'xmc::vec3::cross()']]]
 ];
