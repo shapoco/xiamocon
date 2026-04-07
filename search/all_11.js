32fb9d93f672dcc6d8098df170d975f815a7eaa4
shapoco <40152459+shapoco@users.noreply.github.com>
2026-04-07 05:01:58 +0000

    Deploying to gh-pages from @ shapoco/xiamocon@2bb27d20979ff01e74edfe91ea8ef3f2c9f4baa2 🚀

--- search/all_11.js
+++ search/all_11.js
@@ -3,7 +3,7 @@ var searchData=
   ['textrenderargs_0',['TextRenderArgs',['../structxmc_1_1TextRenderArgs.html',1,'xmc']]],
   ['timer_2ehpp_1',['timer.hpp',['../timer_8hpp.html',1,'']]],
   ['timercallback_2',['TimerCallback',['../timer_8hpp.html#a819a3a9265ca42d5e9c4fc1f40822ab1',1,'xmc']]],
-  ['toeuler_3',['toEuler',['../structxmc_1_1quat.html#ac7a8e6ccaa5f685bfd80ccc48ae4f485',1,'xmc::quat']]],
+  ['toeuler_3',['toEuler',['../structxmc_1_1quat.html#abbc3fe14fa1ef861413caeb7006fb492',1,'xmc::quat']]],
   ['tone_4',['Tone',['../classxmc_1_1audio_1_1Tone.html',1,'xmc::audio']]],
   ['tone_2ehpp_5',['tone.hpp',['../tone_8hpp.html',1,'']]],
   ['transform_6',['transform',['../structxmc_1_1mat4.html#a83887b1bba6e40c1182f02ef5af90b10',1,'xmc::mat4']]],
--- search/functions_e.js
+++ search/functions_e.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['toeuler_0',['toEuler',['../structxmc_1_1quat.html#ac7a8e6ccaa5f685bfd80ccc48ae4f485',1,'xmc::quat']]],
+  ['toeuler_0',['toEuler',['../structxmc_1_1quat.html#abbc3fe14fa1ef861413caeb7006fb492',1,'xmc::quat']]],
   ['transform_1',['transform',['../structxmc_1_1mat4.html#a83887b1bba6e40c1182f02ef5af90b10',1,'xmc::mat4']]],
   ['truncate_2',['truncate',['../classxmc_1_1fs_1_1File.html#a88a3927b1d93dbf60e4ca06a2452040d',1,'xmc::fs::File']]],
   ['tryreadall_3',['tryReadAll',['../ioex_8hpp.html#ac93c2070feda2bf52825abaf2eab7756',1,'xmc::ioex']]]
