63ee953f4e81ea8014c5c80ea838b772d334e8bc
shapoco <40152459+shapoco@users.noreply.github.com>
2026-05-14 15:48:52 +0900

    fix character image for shapo_jump

--- cpp/example/shapo_jump/convert_materials.sh
+++ cpp/example/shapo_jump/convert_materials.sh
@@ -9,20 +9,17 @@ OUT_DIR="$(pwd)/include"
 ${IMG2CPP} \
     ${MATERIAL_DIR}/bmp_bg.png \
     ${OUT_DIR}/bmp_bg.hpp \
-    --dither diffusion \
     --format argb4444 \
     --key-color 808080
 
 ${IMG2CPP} \
     ${MATERIAL_DIR}/bmp_chara.png \
     ${OUT_DIR}/bmp_chara.hpp \
-    --dither diffusion \
     --format argb4444 \
     --key-color 808080
 
 ${IMG2CPP} \
     ${MATERIAL_DIR}/bmp_shapo.png \
     ${OUT_DIR}/bmp_shapo.hpp \
-    --dither diffusion \
     --format argb4444 \
     --key-color 808080
